e78b6c2f7e714ce86ecd645258ca85b1bc0c4d89
MinhNguyenETH <minhnguyen.eth@gmail.com>
2026-05-27 23:23:09 +0700

    refactor: update Vite configuration to include Tailwind CSS plugin and adjust base path

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // Đổi 'car-sale-web' thành tên GitHub repo của bạn
-  base: '/car-sale-web/',
-})
